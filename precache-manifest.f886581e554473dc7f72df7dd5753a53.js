22c9f2c503efa8bb370e8df95475a3b68cd90174
divyanshrana <divyansh.rana12@gmail.com>
2020-07-25 07:01:59 +0530

    Updates

---
+++ precache-manifest.f886581e554473dc7f72df7dd5753a53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ede9e22fc7447701be5cd609227d57c",
+    "url": "/notepad/index.html"
+  },
+  {
+    "revision": "a58628376a843002b3a0",
+    "url": "/notepad/static/css/main.b13d3a8d.chunk.css"
+  },
+  {
+    "revision": "70bf9050c5345279a63e",
+    "url": "/notepad/static/js/2.cea82405.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/notepad/static/js/2.cea82405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58628376a843002b3a0",
+    "url": "/notepad/static/js/main.7dce54a4.chunk.js"
+  },
+  {
+    "revision": "cae7504569adfb7f181f",
+    "url": "/notepad/static/js/runtime-main.9c8e3f8c.js"
+  },
+  {
+    "revision": "1466281417e7844c31ca6e2e86f76f67",
+    "url": "/notepad/static/media/check.14662814.png"
+  }
+]);
